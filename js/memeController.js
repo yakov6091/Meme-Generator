c316155ec5710d5530a56470368cd70bca6a0bb7
Yakov <yakov6091@gmail.com>
2025-04-25 10:56:28 +0300

    Add memeService.js and get meme

--- js/memeController.js
+++ js/memeController.js
@@ -5,17 +5,29 @@ let gCtx
 function onInit() {
     gElCanvas = document.querySelector('canvas')
     gCtx = gElCanvas.getContext('2d')
-
-
     renderMeme()
 }
 
 function renderMeme() {
+    const meme = getMeme()
     const img = new Image()
     img.src = 'img/1.jpg'
+    // const text = document.querySelector('.meme-text').value
+
     img.onload = () => {
         gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height)
-    }
 
+        meme.lines.forEach(line => {
+            gCtx.font = `${line.size}px`;
+            gCtx.fillStyle = line.color;
+            gCtx.strokeStyle = 'blue';
+            gCtx.textAlign = 'center';
+
+            gCtx.fillText(line.txt, gElCanvas.width / 2, 50);
+            gCtx.strokeText(line.txt, gElCanvas.width / 2, 50);
+        })
+
+
+    }
 
 }
---
+++ js/services/memeService.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var gMeme = {
+    selectedImgId: 1,
+    selectedLineIdx: 0,
+    lines: [
+        {
+            txt: 'I eat pizza',
+            size: 20,
+            color: 'red'
+        }
+    ]
+
+}
+
+function getMeme() {
+    return gMeme
+}
